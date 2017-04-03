76e0be8b9ec2701e88eb4597a6ed1537685dae55
DaveGut <djgutheinz@outlook.com>
2017-04-03 15:25:24 -0500

    Add files via upload

--- TP-LinkServerLite.js
+++ TP-LinkServerLite.js
@@ -1,10 +1,14 @@
 /*
+TP-linkServerLite.js V1.2.
 This is a lite version of a node.js server supporting TP-Link Devices.  This node server will:
 a.  receive raw TP-Link device commands from SmartThings.
 b.  encrypt and then send the command to the TP-Link device.
 c.  return the response raw data to the SmartThings from the TP-Link Device.
 History:
 03-13-2017 - Initial release with name TP-LinkServerLite.js
+04-02-2017 - Ver 1.2.  Changed TCP timeout to 2 secs. Added error message that 
+             will be sent to SmartThings if TCP timeout is attained indicating a 
+             bulb off-line. Integrated TCP functions into onRequest. 
 */
 var http = require('http')
 var net = require('net')
@@ -12,60 +16,37 @@ var server = http.createServer(onRequest)
 server.listen(8082) // port must be same as in "sendCmdtoServer" of groovy files.
 console.log("TP-Link Server - Lite Edition")
 function onRequest(request, response){
-	var msg = ''
 	var deviceIP = request.headers["tplink-iot-ip"]
 	var command = request.headers["command"]
+//Add Bridge Version check here with new request header "bridge-version".
 	console.log(" ")
-	var date = new Date()
-	console.log(date)
+	console.log(new Date())
 	console.log("Sending to IP address: " + deviceIP + " Command: " + command)
-	sendCmd(command)
-	function sendCmd(command) {
-		return new Promise((resolve, reject) => {
-		var socket = send(command)
-		socket.on('data', (data) => {
-		data = decrypt(data.slice(4)).toString('ascii')
-		console.log("Command Response sent to SmartThings!")
-		response.setHeader("cmd-response", data)
-		response.end(msg)
-		socket.end()
-		if (!data.err_code || data.err_code === 0) {
-			resolve(data)
-			} else {
-			let errMsg = data
-			console.log('TP-Link Device TCP error %j' + data)
-			reject(new Error(errMsg))
-			}
-		resolve(data)
-		}).on('timeout', () => {
-			socket.end()
-			let errMsg = 'TP-Link Device TCP timeout'
-			console.error(errMsg)
-			reject(new Error(errMsg))
-		}).on('error', (err) => {
-			console.error('TP-Link Device TCP error')
-			console.trace(err)
-			socket.end()
-			reject(err)
-			})
-		})
-		return
-	}
-	function send(payload) {
+	return new Promise((resolve, reject) => {
 		var socket = net.connect(9999, deviceIP)
 		socket.setKeepAlive(false)
-		socket.setTimeout(0)
+		socket.setTimeout(2000)  // 2 seconds.
    	 	socket.on('connect', () => {
-  			socket.write(encrypt(payload))
+  			socket.write(encrypt(command))
    	 	})
-		socket.on('timeout', () => {
+		socket.on('data', (data) => {
+			data = decrypt(data.slice(4)).toString('ascii')
+			console.log("Command Response sent to SmartThings!")
+			response.setHeader("cmd-response", data)
+			response.end()
 			socket.end()
-		})
-		socket.on('end', () => {
+			resolve(data)
+		}).on('timeout', () => {
 			socket.end()
+			response.setHeader("cmd-response", '{"error":"TCP Timeout"}')
+			response.end()
+			reject('Device TCP timeout')
+		}).on('error', (err) => {
+			socket.end()
+			reject(err)
 		})
-		return socket
-	}
+		
+	})
 	function encrypt(input) {
 		var buf = Buffer.alloc(input.length)
 		var key = 0xAB
---
+++ TP-Link_LB-100_110_v1.2.groovy
@@ -0,0 +1,130 @@
+/*
+TP-Link_LB-100&110 Version 1.2
+
+Copyright 2017 Dave Gutheinz
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+file except in compliance with the License. You may obtain a copy of the License at:
+		http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under 
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
+ANY KIND, either express or implied. See the License for the specific language governing 
+permissions and limitations under the License.
+
+Notes: 
+1.	This Device Handler requires an operating Windows 10 PC (server) that interfaces 
+	to the TP-Link Bulbs.
+2.	This DH will work only with the TP-LinkServerLite.js server node.
+3.	This handler is for the TP-Link LB-100/110 bulbs.  This handler support LB-100 and
+    LB-110 power and bulb state functions.
+Update History
+	03/12/2017 - Created initial rendition.  Version 1.0
+    03/30/2017 - Version 1.2.  Rearranged some functions.  Added color coding to
+                 indicate bulb is turning on or off.  Added some notes.
+    04/02/2017 - Added messaging for bridge to device TCP timeout.  Added new 
+    			 power state indicating these two problems. Created dedicated
+                 call-back for commands versus status refresh.
+*/
+metadata {
+	definition (name: "TP-Link LB-100&110", namespace: "V1.2", author: "Dave Gutheinz") {
+		capability "Switch"
+		capability "Switch Level"
+		capability "refresh"
+	}
+	tiles {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
+				nextState:"turningOff"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
+				nextState:"turningOn"
+				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#efd90f",
+				nextState:"on"
+				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#efd90f",
+				nextState:"off"
+                attributeState "offline", label:'Bulb Offline', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#bc2323",
+                nextState:"turningOn"
+			}
+			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+				attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
+			}
+		}
+		standardTile("refresh", "capability.refresh", width: 2, height: 2,  decoration: "flat") {
+			state ("default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh", backgroundColor:"#ffffff")
+		}         
+		main("switch")
+		details(["switch", "refresh"])
+    }
+}
+preferences {
+	input("deviceIP", "text", title: "Device IP", required: true, displayDuringSetup: true)
+	input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
+}
+def on() {
+	log.info "${device.name} ${device.label}: Turning ON"
+	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
+}
+def off() {
+	log.info "${device.name} ${device.label}: Turning OFF"
+	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
+}
+def setLevel(percentage) {
+	log.info "${device.name} ${device.label}: Setting Brightness to ${percentage}%"
+ 	if(device.latestValue("switch") == "off") {
+		sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice": {"transition_light_state": {"on_off": 1}}}', "onAction")
+        sendEvent(name: "switch", value: "turningOn", isStateChange: true)
+		sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"brightness":${percentage}}}}""", "commandResponse")
+    } else {
+		sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"brightness":${percentage}}}}""", "commandResponse")
+	}
+}
+def refresh(){
+	log.info "Polling ${device.name} ${device.label}"
+	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
+}
+private sendCmdtoServer(command, action){
+	def headers = [:] 
+	headers.put("HOST", "$gatewayIP:8082")   // port 8082 must be same as value in TP-LInkServerLite.js
+	headers.put("tplink-iot-ip", deviceIP)
+	headers.put("command", command)
+	sendHubCommand(new physicalgraph.device.HubAction([
+		headers: headers],
+		device.deviceNetworkId,
+		[callback: action]
+	))
+}
+def onAction(response){
+	log.info "On command response returned from bulb."
+}
+def commandResponse(response){
+	def cmdResponse = parseJson(response.headers["cmd-response"])
+	if (cmdResponse.error == "TCP Timeout") {
+		log.error "$device.name $device.label: $cmdResponse.error"
+ 		sendEvent(name: "switch", value: "offline", isStateChange: true)
+	} else {
+		state =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
+    	parseStatus(state)
+	}
+}
+def refreshResponse(response){
+	def cmdResponse = parseJson(response.headers["cmd-response"])
+	if (cmdResponse.error == "TCP Timeout") {
+		log.error "$device.name $device.label: $cmdResponse.error"
+		sendEvent(name: "switch", value: "offline", isStateChange: true)
+	} else {
+		state = cmdResponse.system.get_sysinfo.light_state
+	    parseStatus(state)
+	}
+}
+def parseStatus(state){
+	def status = state.on_off
+	if (status == 1) {
+		status = "on"
+	} else {
+        status = "off"
+		state = state.dft_on_state
+	}
+	def level = state.brightness
+	log.info "$device.name $device.label: Power: ${status} / Mode: ${mode} / Brightness: ${level}% / Color Temp: ${color_temp}K / Hue: ${hue} / Saturation: ${saturation}"
+	sendEvent(name: "switch", value: status, isStateChange: true)
+	sendEvent(name: "level", value: level, isStateChange: true)
+}
