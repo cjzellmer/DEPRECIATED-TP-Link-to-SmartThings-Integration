56f9d314f4412e1aaf47f61991d1ce1ab8196e0b
DaveGut <djgutheinz@outlook.com>
2017-05-07 15:37:28 -0500

    Add files via upload

--- TP-LinkServer.js
+++ TP-LinkServer.js
@@ -1,5 +1,5 @@
 /*
-TP-linkServer.js V2.0.
+TP-linkServer.js V2.1.
 This is a node.js server supporting TP-Link Devices.  This node server will:
 a.  receive raw TP-Link device commands from SmartThings.
 b.  encrypt and then send the command to the TP-Link device.
@@ -10,6 +10,7 @@ History:
 03-13-2017 - Initial release with name TP-LinkServerLite.js
 04-02-2017 - Ver 1.2.  Changed TCP timeout to 2 secs. Added error message that will be sent to SmartThings if TCP timeout is attained indicating a bulb off-line. Integrated TCP functions into onRequest. 
 04-07-2017	- Ver 2.0.  Added TP-LinkBridge device handler interface.  Updated file name to 'TP-LinkServer.js'.
+04-13-2017 - Ver 2.1.  Updated file to have switch. Added responses.
 */
 //-------------------------------------------------------------------------------
 var http = require('http')
@@ -22,26 +23,34 @@ console.log("TP-Link Server - Lite Edition")
 function onRequest(request, response){
 	console.log(" ")
 	console.log(new Date())
-//-- Communications with SmartThings on node status.  ---------------------------
-//-- Will not be used unless NodeJSBridge DH is installed. ----------------------
-	var bridgeExec = require('child_process').exec
 	var command = request.headers["command"]
-	if(command == "restartPC"){
-		console.log("Restarting PC")
-		response.setHeader("cmd-response", "restartPC")
-		response.end()
-		bridgeExec('shutdown /r /t 005')
-	} else if(command == "pollServer"){
-		console.log("Bridge poll response sent to SmartThings")
-		response.setHeader("cmd-response", "ok")
-		response.end()
-	} else {
-		processRequest(request, response)
+	switch(command) {
+		case "restartPC":
+			var bridgeExec = require('child_process').exec
+			console.log("Restarting PC")
+			response.setHeader("cmd-response", "restartPC")
+			response.end()
+			bridgeExec('shutdown /r /t 005')
+			break
+
+		case "pollServer":
+			console.log("Server poll response sent to SmartThings")
+			response.setHeader("cmd-response", "ok")
+			response.end()
+			break
+
+		case "deviceCommand":
+			processDeviceCommand(request, response)
+			break
+
+		default:
+			console.log("Invalid Command received from SmartThings")
+			response.setHeader("cmd-response", "invalidServerCmd")
 	}
 }
 //-- Response to TP-Link Device Commands ---------------------------------------
-function processRequest(request, response) {
-	var command = request.headers["command"]
+function processDeviceCommand(request, response) {
+	var command = request.headers["tplink-command"]
 	var deviceIP = request.headers["tplink-iot-ip"]
 	console.log("Sending to IP address: " + deviceIP + " Command: " + command)
 //--- Encrypt then send command to device and wait for response. -------
@@ -63,14 +72,13 @@ function processRequest(request, response) {
 //-- If a timeout, send a timeout indication to SmartThings. ------------------
 		}).on('timeout', () => {
 			socket.end()
-			response.setHeader("cmd-response", '{"error":"TCP Timeout"}')
+			response.setHeader("cmd-response", 'TcpTimeout')
 			response.end()
 			reject('Device TCP timeout')
 		}).on('error', (err) => {
 			socket.end()
 			reject(err)
 		})
-		
 	})
 //-- Encrypt the command including a 4 byte TCP header. -----------------------
 	function encrypt(input) {
